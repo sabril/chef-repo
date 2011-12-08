f7324e504f2e00ac38f063f1b0b3a7672e737edf
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2011-12-08 23:54:04 +0700

    first commit

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,11 +1,12 @@
-current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "sabril"
-client_key               "#{current_dir}/sabril.pem"
-validation_client_name   "ittelkom-validator"
-validation_key           "#{current_dir}/ittelkom-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/ittelkom"
+node_name                'syaifulsabril'
+client_key               '/Users/syaifulsabril/chef-repo/.chef/syaifulsabril.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef/validation.pem'
+chef_server_url          'http://67.202.38.172:4000'
 cache_type               'BasicFile'
-cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
-cookbook_path            ["#{current_dir}/../cookbooks"]
+cache_options( :path => '/Users/syaifulsabril/chef-repo/.chef/checksums' )
+
+knife[:aws_access_key_id] = "AKIAJOZHFEZALLKCKUOQ"
+knife[:aws_secret_access_key] = "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"
---
+++ client-config/client.rb
@@ -0,0 +1,4 @@
+log_level        :info
+log_location     STDOUT
+chef_server_url  'https://api.opscode.com/organizations/ittelkom'
+validation_client_name 'ittelkom-validator'
